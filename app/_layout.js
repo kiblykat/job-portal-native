bb0d2914ac85b5c2d2797ebfa63b78394e1a428a
izzat <izzatfadzlon@gmail.com>
2024-04-06 12:01:44 +0800

    41:14 - working commit, comment _layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,25 @@
 import { Stack } from "expo-router";
+// import { useCallback } from "react";
+// import useFonts from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
 
+// SplashScreen.preventAutoHideAsync(); -----this is the cause of the white screen error
 const Layout = () => {
+  // const [fontsLoaded] = useFonts({
+  //   DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+  //   DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+  //   DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  // });
+
+  // //similar to useEffect
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  // if (!fontsLoaded) return null;
+
   return <Stack />;
 };
 
